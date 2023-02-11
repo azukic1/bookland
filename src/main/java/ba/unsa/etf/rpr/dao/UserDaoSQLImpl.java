b8d13b0af6b250eb4b2f51bc6fb1a3a46e16b0e0
Ajla Zukic <azukic1@etf.unsa.ba>
2023-02-11 20:23:39 +0100

    implemented searching method in UserDaoSQLImpl

--- src/main/java/ba/unsa/etf/rpr/dao/UserDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/UserDaoSQLImpl.java
@@ -3,20 +3,42 @@ package ba.unsa.etf.rpr.dao;
 import ba.unsa.etf.rpr.domain.User;
 import ba.unsa.etf.rpr.exceptions.BookException;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class UserDaoSQLImpl extends AbstractDao<User> implements UserDao{
 
     public UserDaoSQLImpl() {
-        super("users");
+        super("Users");
     }
 
     @Override
     public List<User> searchByFirstName(String firstName) throws BookException{
-        return null;
+        String query = "SELECT * FROM Users WHERE first_name=?";
+        try{
+            PreparedStatement stmt = this.getConnection().prepareStatement(query);
+            stmt.setString(1,firstName);
+            ResultSet rs = stmt.executeQuery();
+            ArrayList<User> userList = new ArrayList<>();
+            while(rs.next()) {
+                User u = new User();
+                u.setId(rs.getInt(1));
+                u.setFirstName(firstName);
+                u.setLastName(rs.getString(3));
+                u.setUsername(rs.getString(4));
+                u.setPassword(rs.getString(5));
+                u.setAdministrator(rs.getBoolean(6));
+                userList.add(u);
+            }
+            return userList;
+
+        }catch(SQLException e) {
+            throw new BookException(e.getMessage(),e);
+        }
     }
 
     @Override
