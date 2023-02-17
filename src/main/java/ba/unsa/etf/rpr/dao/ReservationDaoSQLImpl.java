8fec914229ac9fc04c0c898cd216bc9373fcf97c
Ajla Zukic <azukic1@etf.unsa.ba>
2023-02-17 19:37:46 +0100

    implemented 2 methods in ReservationDaoSQLImpl.java

--- src/main/java/ba/unsa/etf/rpr/dao/ReservationDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/ReservationDaoSQLImpl.java
@@ -5,6 +5,7 @@ import ba.unsa.etf.rpr.exceptions.BookException;
 
 import java.sql.ResultSet;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class ReservationDaoSQLImpl extends AbstractDao<Reservation> implements ReservationDao{
 
@@ -13,13 +14,27 @@ public class ReservationDaoSQLImpl extends AbstractDao<Reservation> implements R
     }
 
     @Override
-    public Reservation row2object(ResultSet rs)throws BookException {
-        return null;
+    public Reservation row2object(ResultSet rs) throws BookException {
+
+        try{
+            Reservation r = new Reservation();
+            r.setId(rs.getInt(1));
+            r.setBook(DaoFactory.bookDao().getById(rs.getInt(2)));
+            r.setUser(DaoFactory.userDao().getById(rs.getInt(3)));
+            return r;
+
+        }catch(Exception e){
+            throw new BookException(e.getMessage(),e);
+        }
     }
 
     @Override
-    public Map<String, Object> object2row(Reservation object) throws BookException {
-        return null;
+    public Map<String, Object> object2row(Reservation object) {
+        Map<String, Object> r = new TreeMap<String,Object>();
+        r.put("id",object.getId());
+        r.put("idUser",object.getUser().getId());
+        r.put("idBook",object.getBook().getId());
+        return r;
     }
 
 }
