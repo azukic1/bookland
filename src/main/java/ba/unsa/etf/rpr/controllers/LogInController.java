b6c64d981da52971595f1102dbdd2e467e3abd6b
Ajla Zukic <azukic1@etf.unsa.ba>
2023-02-19 00:17:00 +0100

    implemented signUpAction in LogInController.java

--- src/main/java/ba/unsa/etf/rpr/controllers/LogInController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/LogInController.java
@@ -1,12 +1,17 @@
 package ba.unsa.etf.rpr.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 
 import java.io.IOException;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class LogInController {
 
@@ -20,6 +25,14 @@ public class LogInController {
     }
 
     public void signUpAction(ActionEvent actionEvent) throws IOException {
+        Stage stage = new Stage();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/signup.fxml"));
+        SignUpController controller = new SignUpController();
+        loader.setController(controller);
+        stage.setTitle("Sign Up");
+        stage.setScene(new Scene(loader.load(),USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        stage.setResizable(false);
+        stage.show();
 
     }
 
