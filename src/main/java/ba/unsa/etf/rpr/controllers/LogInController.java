50b5146865586a70f4f5daf83d0ed0205045f801
Ajla Zukic <azukic1@etf.unsa.ba>
2023-02-19 00:02:17 +0100

    add LogInController.java

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/LogInController.java
@@ -0,0 +1,27 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+
+import java.io.IOException;
+
+public class LogInController {
+
+    public TextField usernameId;
+    public PasswordField passwordId;
+    public Button loginBttn;
+    public Button signupBttn;
+
+    public LogInController() {
+
+    }
+
+    public void signUpAction(ActionEvent actionEvent) throws IOException {
+
+    }
+
+
+}
