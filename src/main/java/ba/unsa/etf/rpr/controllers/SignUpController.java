a44f786f475aea0bc2a65a582a00ce30b592401f
Ajla Zukic <azukic1@etf.unsa.ba>
2023-02-19 00:06:02 +0100

    add SignUpController.java

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/SignUpController.java
@@ -0,0 +1,27 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class SignUpController {
+
+    public TextField firstNameId;
+    public TextField lastNameId;
+    public TextField usernameId;
+    public PasswordField passwordId;
+
+    public Button signUpBttn;
+    public Label neispravanPasswordId;
+    public Label neispravanUsernameId;
+
+    public SignUpController() {
+
+    }
+
+    public void signUpAction(ActionEvent actionEvent) {
+
+    }
+}
