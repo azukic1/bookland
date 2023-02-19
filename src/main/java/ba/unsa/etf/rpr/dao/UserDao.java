a88cd98efb3899097cf92a4a5c2d55c9d3a5f1ef
Ajla Zukic <azukic1@etf.unsa.ba>
2023-02-19 14:08:01 +0100

    javadoc update

--- src/main/java/ba/unsa/etf/rpr/dao/UserDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/UserDao.java
@@ -6,10 +6,27 @@ import ba.unsa.etf.rpr.exceptions.BookException;
 
 import java.util.List;
 
+/**
+ * Dao interface for User domain bean
+ *
+ * @author Ajla Zukic
+ */
 public interface UserDao extends Dao<User> {
 
+    /**
+     *
+     * @param firstName
+     * @return list of users
+     * @throws BookException
+     */
     List<User> searchByFirstName(String firstName) throws BookException;
 
+    /**
+     *
+     * @param lastName
+     * @return list of users
+     * @throws BookException
+     */
     List<User> searchByLastName(String lastName) throws BookException;
 
 
