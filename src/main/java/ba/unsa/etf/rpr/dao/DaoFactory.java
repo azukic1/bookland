a820822f181398891c918803588d3eda99515687
Ajla Zukic <azukic1@etf.unsa.ba>
2023-02-19 14:15:52 +0100

    javadoc update

--- src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
+++ src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
@@ -1,5 +1,10 @@
 package ba.unsa.etf.rpr.dao;
 
+/**
+ * Factory method for singleton implementation of DAOs
+ *
+ * @author Ajla Zukic
+ */
 public class DaoFactory {
     private static final BookDao bookDao =  BookDaoSQLImpl.getInstance();
     private static final ReservationDao reservationDao = ReservationDaoSQLImpl.getInstance();
