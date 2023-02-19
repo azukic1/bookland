139ce8c113955b07d96f9ffab6faf0e9c32f25d4
Ajla Zukic <azukic1@etf.unsa.ba>
2023-02-19 23:06:55 +0100

    add AdminHomeController

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/AdminHomeController.java
@@ -0,0 +1,20 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
+public class AdminHomeController {
+
+    public Button usersBttn;
+    public Button booksBttn;
+
+    public AdminHomeController() {
+
+    }
+
+    public void booksBttnAction(ActionEvent actionEvent) {
+    }
+
+    public void usersBttnAction(ActionEvent actionEvent) {
+    }
+}
