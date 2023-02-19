e2f6000714e15c07bd6fab4546eba09456769efa
Ajla Zukic <azukic1@etf.unsa.ba>
2023-02-19 22:55:57 +0100

    add UserHomeController.java

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/UserHomeController.java
@@ -0,0 +1,22 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class UserHomeController {
+    public TextField titleId;
+    public TextField authorId;
+    public Button authorBttn;
+    public Button titleBttn;
+
+    public UserHomeController() {
+
+    }
+
+    public void searchByAuthorAction(ActionEvent actionEvent) {
+    }
+
+    public void searchByTitleAction(ActionEvent actionEvent) {
+    }
+}
