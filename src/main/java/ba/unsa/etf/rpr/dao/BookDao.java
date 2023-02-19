baeb27f685b8d8a5b683ab4a339a0b14a7eacb51
Ajla Zukic <azukic1@etf.unsa.ba>
2023-02-19 13:16:43 +0100

    javadoc update

--- src/main/java/ba/unsa/etf/rpr/dao/BookDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/BookDao.java
@@ -4,10 +4,26 @@ import ba.unsa.etf.rpr.domain.Book;
 import ba.unsa.etf.rpr.exceptions.BookException;
 
 import java.util.List;
+/**
+ * Dao interface for Book domain bean
+ *
+ * @author Ajla Zukic
+ */
 
 public interface BookDao extends Dao<Book> {
 
+    /**
+     * @param author
+     * @return list of books
+     * @throws BookException
+     */
     List<Book> searchByAuthor(String author) throws BookException;
+
+    /**
+     * @param title
+     * @return list of books
+     * @throws BookException
+     */
     List<Book> searchByTitle(String title) throws BookException;
 
 }
--- src/main/java/ba/unsa/etf/rpr/dao/ReservationDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/ReservationDao.java
@@ -2,5 +2,11 @@ package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Reservation;
 
+/**
+ * Dao interface for Reservation domain bean
+ *
+ * @author Ajla Zukic
+ */
+
 public interface ReservationDao extends Dao<Reservation> {
 }
