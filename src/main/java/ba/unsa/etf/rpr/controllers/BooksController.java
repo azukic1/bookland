3c7d29ec945346b397c0a1f03714c03f36970f30
Ajla Zukic <azukic1@etf.unsa.ba>
2023-02-20 17:37:20 +0100

    add BooksController

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/BooksController.java
@@ -0,0 +1,26 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
+public class BooksController {
+    public Button addBookBttn;
+    public Button deleteBookBttn;
+    public Button cancelBttn;
+    public ListView listId;
+
+    public BooksController() {
+
+    }
+
+    public void addBookAction(ActionEvent actionEvent) {
+    }
+
+    public void deleteBookAction(ActionEvent actionEvent) {
+    }
+
+    public void cancelAction(ActionEvent actionEvent) {
+        
+    }
+}
