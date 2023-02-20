bf794322bd5eebce466aebfbef11a878e1489ca9
Ajla Zukic <azukic1@etf.unsa.ba>
2023-02-20 17:46:11 +0100

    list of users

--- src/main/java/ba/unsa/etf/rpr/controllers/AdminHomeController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/AdminHomeController.java
@@ -31,6 +31,13 @@ public class AdminHomeController {
     }
 
     public void usersBttnAction(ActionEvent actionEvent) throws IOException {
-
+        Stage stage = new Stage();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/users.fxml"));
+        UsersController controller = new UsersController();
+        loader.setController(controller);
+        stage.setTitle("");
+        stage.setScene(new Scene(loader.load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        stage.setResizable(false);
+        stage.show();
     }
 }
