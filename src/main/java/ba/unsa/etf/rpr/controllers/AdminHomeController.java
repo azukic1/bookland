db8fade61ae968afc5f07cbe1fc4a4ad345d120b
Ajla Zukic <azukic1@etf.unsa.ba>
2023-02-20 17:45:22 +0100

    list of books

--- src/main/java/ba/unsa/etf/rpr/controllers/AdminHomeController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/AdminHomeController.java
@@ -1,7 +1,14 @@
 package ba.unsa.etf.rpr.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class AdminHomeController {
 
@@ -12,9 +19,18 @@ public class AdminHomeController {
 
     }
 
-    public void booksBttnAction(ActionEvent actionEvent) {
+    public void booksBttnAction(ActionEvent actionEvent) throws IOException{
+        Stage stage = new Stage();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/books.fxml"));
+        BooksController controller = new BooksController();
+        loader.setController(controller);
+        stage.setTitle("");
+        stage.setScene(new Scene(loader.load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        stage.setResizable(false);
+        stage.show();
     }
 
-    public void usersBttnAction(ActionEvent actionEvent) {
+    public void usersBttnAction(ActionEvent actionEvent) throws IOException {
+
     }
 }
