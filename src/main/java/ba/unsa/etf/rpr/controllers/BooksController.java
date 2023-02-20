b8e21dca63aa23de73783ce32e7681352694d820
Ajla Zukic <azukic1@etf.unsa.ba>
2023-02-20 18:07:52 +0100

    add list of books in BooksController

--- src/main/java/ba/unsa/etf/rpr/controllers/BooksController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/BooksController.java
@@ -1,9 +1,18 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.business.BookManager;
+import ba.unsa.etf.rpr.domain.Book;
+import ba.unsa.etf.rpr.domain.User;
+import ba.unsa.etf.rpr.exceptions.BookException;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BooksController {
     public Button addBookBttn;
@@ -11,14 +20,28 @@ public class BooksController {
     public Button cancelBttn;
     public ListView listId;
 
+    private final BookManager bookManager = new BookManager();
     public BooksController() {
 
     }
+    @FXML
+    public void initialize() throws BookException {
+        List<Book> allBooks = FXCollections.observableList(bookManager.getAll());
+        List<String> books = new ArrayList<>();
+        for(int i = 0; i<allBooks.size(); i++) {
+                books.add(allBooks.get(i).getTitle() + " " + allBooks.get(i).getAuthor());
 
-    public void addBookAction(ActionEvent actionEvent) {
+        }
+        listId.getItems().addAll(books);
     }
 
-    public void deleteBookAction(ActionEvent actionEvent) {
+
+    public void addBookAction(ActionEvent actionEvent) {
+
+    }
+
+    public void deleteBookAction(ActionEvent actionEvent) throws BookException {
+
     }
     public void cancelAction(ActionEvent actionEvent) {
         Stage stage = (Stage) listId.getScene().getWindow();
