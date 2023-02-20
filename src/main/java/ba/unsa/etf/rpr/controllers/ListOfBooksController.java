c84fc69a1e32540c58edbcaebec3905824d40a8e
Ajla Zukic <azukic1@etf.unsa.ba>
2023-02-21 00:45:54 +0100

    add ListOfBooksController.java

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/ListOfBooksController.java
@@ -0,0 +1,39 @@
+package ba.unsa.etf.rpr.controllers;
+
+import ba.unsa.etf.rpr.business.BookManager;
+import ba.unsa.etf.rpr.domain.Book;
+import ba.unsa.etf.rpr.exceptions.BookException;
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListOfBooksController {
+    public ListView listId;
+    public Button borrowBookBttn;
+    public Button cancelBttn;
+    private final BookManager bookManager = new BookManager();
+
+    @FXML
+    public void initialize() throws BookException {
+        List<Book> allBooks = FXCollections.observableList(bookManager.getAll());
+        List<String> books = new ArrayList<>();
+        for(int i = 0; i<allBooks.size(); i++) {
+            books.add(allBooks.get(i).getTitle() + " " + allBooks.get(i).getAuthor());
+
+        }
+        listId.getItems().addAll(books);
+    }
+
+    public void borrowBookAction(ActionEvent actionEvent) {
+
+    }
+
+    public void cancelAction(ActionEvent actionEvent) {
+
+    }
+}
