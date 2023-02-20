b30a2187e7332e88efe17729838664191b0a4e90
Ajla Zukic <azukic1@etf.unsa.ba>
2023-02-21 00:47:32 +0100

    now user can see list of books

--- src/main/java/ba/unsa/etf/rpr/controllers/UserHomeController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/UserHomeController.java
@@ -1,22 +1,62 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.business.BookManager;
+import ba.unsa.etf.rpr.domain.Book;
+import ba.unsa.etf.rpr.domain.User;
+import ba.unsa.etf.rpr.exceptions.BookException;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class UserHomeController {
     public TextField titleId;
     public TextField authorId;
     public Button authorBttn;
     public Button titleBttn;
+    public Button booksId;
+    String author;
 
     public UserHomeController() {
 
     }
 
-    public void searchByAuthorAction(ActionEvent actionEvent) {
+    public UserHomeController(String author) {
+        this.author = author;
+    }
+
+    @FXML
+    public void initialize() {
+
+    }
+
+    public void searchByAuthorAction(ActionEvent actionEvent) throws IOException, BookException {
+
     }
 
     public void searchByTitleAction(ActionEvent actionEvent) {
+
+    }
+
+    public void browseAllAction(ActionEvent actionEvent) throws IOException {
+        Stage stage = new Stage();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/listOfBooks.fxml"));
+        ListOfBooksController controller = new ListOfBooksController();
+        loader.setController(controller);
+        stage.setTitle("");
+        stage.setScene(new Scene(loader.load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        stage.setResizable(false);
+        stage.show();
     }
 }
