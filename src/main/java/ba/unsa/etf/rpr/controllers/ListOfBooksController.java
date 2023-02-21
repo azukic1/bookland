3b8a547421b0637ec855949ad5bd101b2e184b11
Ajla Zukic <azukic1@etf.unsa.ba>
2023-02-21 11:10:11 +0100

    small changes

--- src/main/java/ba/unsa/etf/rpr/controllers/ListOfBooksController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/ListOfBooksController.java
@@ -1,7 +1,11 @@
 package ba.unsa.etf.rpr.controllers;
 
 import ba.unsa.etf.rpr.business.BookManager;
+import ba.unsa.etf.rpr.business.ReservationManager;
+import ba.unsa.etf.rpr.business.UserManager;
 import ba.unsa.etf.rpr.domain.Book;
+import ba.unsa.etf.rpr.domain.Reservation;
+import ba.unsa.etf.rpr.domain.User;
 import ba.unsa.etf.rpr.exceptions.BookException;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
@@ -17,6 +21,13 @@ public class ListOfBooksController {
     public ListView listId;
     public Button borrowBookBttn;
     public Button cancelBttn;
+    private int id;
+
+    public ListOfBooksController(int id) {
+        this.id = id;
+    }
+    public ListOfBooksController() {}
+
     private final BookManager bookManager = new BookManager();
 
     @FXML
@@ -28,10 +39,32 @@ public class ListOfBooksController {
 
         }
         listId.getItems().addAll(books);
+        System.out.println(id);
     }
+    private final ReservationManager reservationManager = new ReservationManager();
+    private final UserManager userManager = new UserManager();
+    //private final BookManager bookManager = new BookManager();
 
-    public void borrowBookAction(ActionEvent actionEvent) {
-
+    public void borrowBookAction(ActionEvent actionEvent) throws BookException {
+        int in = listId.getSelectionModel().getSelectedIndex();
+        Reservation r = new Reservation();
+        List<User> users = FXCollections.observableList(userManager.getAll());
+        for(int i = 0; i < users.size(); i++) {
+            if(users.get(i).getId() == 5) r.setUser(users.get(i));
+        }
+        int a=0;
+        List<Book> books = FXCollections.observableList(bookManager.getAll());
+        for (int i = 0; i < books.size(); i++) {
+            if(books.get(i).getId() == books.get(in).getId()) {
+                r.setBook(books.get(i));
+                a = books.get(in).getId();
+            }
+        }
+        reservationManager.add(r);
+        int n = books.get(in).getAvailableCopies();
+        books.get(in).setAvailableCopies(n-1);
+        bookManager.update(books.get(in));
+        System.out.println(id);
     }
 
     public void cancelAction(ActionEvent actionEvent) {
