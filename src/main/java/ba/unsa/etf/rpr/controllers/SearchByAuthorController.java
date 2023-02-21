c1b5849cb9900dd1ad4f8cfba5860d504019208c
Ajla Zukic <azukic1@etf.unsa.ba>
2023-02-21 01:01:11 +0100

    cancel button

--- src/main/java/ba/unsa/etf/rpr/controllers/SearchByAuthorController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/SearchByAuthorController.java
@@ -8,6 +8,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.stage.Stage;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -38,6 +39,7 @@ public class SearchByAuthorController {
     }
 
     public void cancelAction(ActionEvent actionEvent) {
-
+        Stage stage = (Stage) listId.getScene().getWindow();
+        stage.close();
     }
 }
