f33edf425052fa8fe621d73a36322855921116c3
Ajla Zukic <azukic1@etf.unsa.ba>
2023-02-21 17:02:19 +0100

    javadoc update

--- src/main/java/ba/unsa/etf/rpr/exceptions/BookException.java
+++ src/main/java/ba/unsa/etf/rpr/exceptions/BookException.java
@@ -1,5 +1,10 @@
 package ba.unsa.etf.rpr.exceptions;
 
+/**
+ * use-defined exception
+ *
+ * @author Ajla Zukic
+ */
 public class BookException extends Exception{
 
     public BookException(String msg, Exception reason){
